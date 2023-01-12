273abba27158b800d5f4d5bc12db6da87da96e36
SergSvet87 <svetlichnij1905@gmail.com>
2023-01-12 23:53:02 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,92 @@
+const burgerMenu = document.querySelector('.burger-menu');
+const menuMobile = document.querySelector('.menu-mobile');
+
+burgerMenu.addEventListener('click', () => {
+  menuMobile.classList.toggle('menu-mobile_open')
+})
+
+const swiper = new Swiper('.swiper', {
+
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-right',
+    prevEl: '.swiper-button-left',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    }
+  }
+});
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.23163498124435, lng: -8.628473092139991 },
+    zoom: 13,
+    styles: [
+      {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [{ "saturation": "-100" }]
+      },
+      {
+        " featureType": "administrative.province",
+        "elementType": "all",
+        "stylers": [{ "visibility": "off" }]
+      },
+      {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [{ "saturation": -100 }, { "lightness": 65 }, { "visibility": "on" }]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [{ "saturation": -100 }, { "lightness": "50" }, { "visibility": "simplified" }]
+      },
+      {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [{ "saturation": "-100" }]
+      },
+      {
+        " featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [{ "visibility": "simplified" }]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [{ "lightness": "30" }]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [{ "lightness": "30" }]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [{ "saturation": -100 }, { "visibility": "simplified" }]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{ "hue": "#ffff00" }, { "lightness": -25 }, { "saturation": -97 }]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [{ "lightness": -25 }, { "saturation": -100 }]
+      }
+    ]
+  });
+}
+
+window.initMap = initMap;
